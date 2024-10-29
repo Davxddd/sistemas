29b195600f7b9e980485f2db38480e0d95b0023f
davdd <dalvaradom4@upao.edu.pe>
2024-10-29 10:19:45 -0500

    davod

---
+++ src/Laboratorio11.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Laboratorio11 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Ingrese la cantidad de estudiantes:");
+        int cantidadEstudiantes = Integer.parseInt(br.readLine());
+        String[] arregloCadenas = new String[cantidadEstudiantes];
+
+        for (int i = 0; i < arregloCadenas.length; i++) {
+            System.out.print("Ingresa el nombre del estudiante:");
+            String value = br.readLine();
+            GuardarValor(arregloCadenas, i, value);
+
+            System.out.println("Debes ingresar las 4 notas del estudiante");
+            int[] notas = new int[4];
+            for (int j = 0; j < notas.length; j++) {
+                int nota = Integer.parseInt(br.readLine());
+                notas[j] = nota;
+            }
+            int promedio = CalcularPromedio(notas);
+            System.out.println("El promedio de notas del estuiante es:" + promedio);
+        }
+
+
+
+        // Validaciones:
+        // Las notas son de 0 a 20
+        // Los alumnos deben tener al menos nombre y apellido
+
+        // PASO 1: INGRESA LA CANTIDAD DE ESTUDIANTES
+        // PASO 2: INGRESA LAS NOTAS DEL ESTUDIANTE
+        // PASO 3: CALCULA EL PROMEDIO DEL ESTUDIANTE
+        // PASO 3: CALCULA LA NOTA MAS ALTA
+        // PASO 3: CALCULA LA NOTA MAS BAJA
+        // PASO 3: CALCULA LA MEDIA
+        ImprimirObjetos(arregloCadenas);
+    }
+
+    private static int CalcularPromedio(int[] notas) {
+        int sumaTotal = 0;
+        for (int i = 0; i < notas.length; i++) {
+            sumaTotal += notas[i];
+        }
+        return sumaTotal / notas.length;
+    }
+
+    private static void GuardarValor(String[] arreglo, int position, String value) {
+        if (arreglo.length <= position) return;
+        arreglo[position] = value;
+    }
+
+    private static void ImprimirObjetos(String[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo[i]);
+        }
+    }
+
+    private static void ImprimirObjetos(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo[i]);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Ingrese la cantidad de estudiantes: ");
+        int Ce = Integer.parseInt(br.readLine());
+
+        String[] nombres = new String[Ce];
+        double[] notas = new double[Ce];
+
+        for (int i = 0; i < Ce; i++) {
+            System.out.print("Ingrese el nombre del estudiante " + (i + 1) + ": ");
+            String nombre = br.readLine();
+            System.out.print("Ingrese el apellido del estudiante " + (i + 1) + ": ");
+            String apellido = br.readLine();
+
+            if (nombre.length() == 0 || apellido.length() == 0) {
+                System.out.println("Ingrese sus nombres y sus apellidos");
+                i--;
+                continue;
+            }
+            nombres[i] = nombre + " " + apellido;
+            double nota;
+            do {
+                System.out.print("Ingrese la nota del estudiante " + (i + 1) + ": ");
+                nota = Double.parseDouble(br.readLine());
+            } while (nota < 0 || nota > 20);
+
+            notas[i] = nota;
+        }
+
+        double SumNot = 0;
+        double notAlta = notas[0];
+        double notBaja = notas[0];
+
+        for (double nota : notas) {
+            SumNot += nota;
+            if (nota > notAlta) {
+                notAlta = nota;
+            }
+            if (nota < notBaja) {
+                notBaja = nota;
+            }
+        }
+
+        double promedio = SumNot / Ce;
+        System.out.println("El promedio de notas es: " + promedio);
+        System.out.println("La nota más alta es: " + notAlta);
+        System.out.println("La nota más baja es: " + notBaja);
+    }
+}
